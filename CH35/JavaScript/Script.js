0160757704440732479e220ee4a59a47fec1ac35
Fercarmonahuerta <fernandacarmonahuerta@gmail.com>
2023-12-09 07:02:35 -0600

    subir archivos al repositorio

---
+++ CH35/JavaScript/Script.js
@@ -0,0 +1,47 @@
+//comentarios de una sola línea
+/*comentarios 
+multinínea */
+
+//alert("Hola mundo");
+
+//console.log("Este es un mensaje para mi consola");
+
+/*var nombre = prompt("ingresa tu nombre");
+alert("Hola, "  + nombre  + " Bienvendio a la ch35");*/
+
+/*var numero1 = parseInt(prompt("Ingresa el primer número:"))
+var numero2 = parseInt(prompt("Ingresa el segundo número:"))
+
+console.log("suma:" + (numero1 + numero2));
+console.log("resta: " + (numero1-numero2));
+console.log("multiplicación:" + (numero1 * numero2));
+console.log("división: " + (numero1/numero2));*/
+
+/*var edad = prompt("Ingresa tu edad:");
+
+if (edad >= 18) {
+    alert("Bienvenido. ¡Disfruta de la página!");
+} else {
+    alert("Lo siento, debes ser mayor de 18 años para acceder. Acceso denegado.");
+}*/
+
+//TIPOS DE DATOS Y VARIABLES
+//scope es el alcance de nuestras variables
+
+/*var, let, const la diferencia es el alcance 
+var tiene un alcance global
+let tiene un alcnace local
+const se mantiene sin ningun cambio*/
+
+/*string:cadena de texto (lleva comillas)
+number: numero
+noolena: true/false
+null:nulo
+undefinied:no esta asigando o definido*/
+
+let nombre = "Daniel";
+let invitadosExtras = 2;
+let esMaryorEdad = true;
+let edad = 24;
+let invitadoEspecial = null;
+let horaDeSalida = undefined;
